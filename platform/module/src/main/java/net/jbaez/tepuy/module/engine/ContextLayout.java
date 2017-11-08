68cc497fc6e8e2641e2d7feb53150916a6682edb
Jesús Baez <jesus@jbaez.net>
2017-11-08 21:48:23 +0100

    Se crean las interfaces base del sistema de modulos 	modificado:    platform/module/pom.xml 	nuevo archivo: platform/module/src/main/java/net/jbaez/tepuy/module/engine/ClassLoaderSupplier.java 	nuevo archivo: platform/module/src/main/java/net/jbaez/tepuy/module/engine/ContextLayout.java 	nuevo archivo: platform/module/src/main/java/net/jbaez/tepuy/module/engine/MainContextSupplier.java 	modificado:    pom.xml

---
+++ platform/module/src/main/java/net/jbaez/tepuy/module/engine/ContextLayout.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Jesus Baez
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ ******************************************************************************/
+
+package net.jbaez.tepuy.module.engine;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * <p> Define la manera como se debe organizar
+ * los contextos de los modulos
+ * @author Jesus Baez
+ */
+public interface ContextLayout 
+{
+
+  /**
+   * <p>Inicializa el layout 
+   * @param ctx Context raiz de la aplicacion
+   */
+  void initialize(ApplicationContext ctx);
+  /**
+   * <p> Anade el modulo en el layout 
+   * @param module {@link Module} a anadir
+   */
+  void addModule(Module module);
+}
---
+++ platform/module/src/main/java/net/jbaez/tepuy/module/engine/MainContextSupplier.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Jesus Baez
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ ******************************************************************************/
+
+package net.jbaez.tepuy.module.engine;
+
+import java.util.function.Supplier;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * <p> Interfaz utilizada para suministrar
+ * el contexto raiz del sistema de modulos
+ * @author Jesus Baez
+ */
+public interface MainContextSupplier extends Supplier<ApplicationContext> {
+
+  /**
+   * {@inheritDoc}
+   * @see java.util.function.Supplier#get()
+   */
+  @Override
+  ApplicationContext get();
+}
